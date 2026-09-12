ae2bc3ae90cb07800cdd2122ca193058124e6b18
Rumit Maharjan <rumitaus@gmail.com>
2026-09-12 17:41:04 +1000

    Add abstract Cell base class

---
+++ src/Cell.java
@@ -0,0 +1,13 @@
+public abstract class  Cell {
+    private boolean alive;
+
+    public Cell(boolean alive){
+        this.alive = alive;
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
+
+    public abstract boolean nextState(int liveNeighbours);
+}
